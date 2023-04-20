981eb1a9073c4026e37a389e04abd57e9398a297
JiangYangfan <jiangyangfan@ifugle.com>
2023-04-20 22:32:47 +0800

    新增：脚本

--- nodes/clean.sh
+++ nodes/clean.sh
@@ -1,4 +1,4 @@
 docker ps -a | grep redis | awk '{print $1}' | xargs docker stop
 docker ps -a | grep redis | awk '{print $1}' | xargs docker rm
-docker netwoke rm redis-net
+docker network rm redis-net
 rm -rf /home/redis
