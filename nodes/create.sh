ee7ddf883b8717aa1dcdef6fdb758c5d205d09cb
JiangYangfan <jiangyangfan@ifugle.com>
2023-04-20 22:36:45 +0800

    新增：脚本

--- nodes/create.sh
+++ nodes/create.sh
@@ -4,6 +4,8 @@
 ip=$(ifconfig -a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:" | grep 192 | head -n 1)
 # Docker网桥IP
 #ip=$(ifconfig -a | grep docker -A 2 | grep inet | grep -v inet6 | awk '{print $2}')
+echo "===== IP ${ip} ====="
+
 
 for port in {6401..6406}; do
 	mkdir -p /home/redis/node-${port}/conf
@@ -25,30 +27,31 @@ cluster-announce-port ${port}
 cluster-announce-bus-port 1${port}
 EOF
 done
-
+echo "===== Config Done ====="
 
 #### Docker #####
 
 # 清理
 for port in {6401..6406}; do
-   docker container stop redis-${port}
-   docker container rm redis-${port}
+	docker container stop redis-${port}
+	docker container rm redis-${port}
 done
 docker network rm redis-net
+echo "===== Clean Done ====="
 
 # 创建
 docker network create redis-net
 for port in {6401..6406}; do
-   docker run -it -u 0 -d \
-      -p ${port}:${port} -p 1${port}:1${port} \
-      --privileged=true \
-      -v /home/redis/node-${port}/conf/redis.conf:/usr/local/etc/redis/redis.conf \
-      -v /home/redis/node-${port}/data:/data \
-      --restart always --name redis-${port} --net redis-net \
-      --sysctl net.core.somaxconn=1024 \
-      redis:5.0 redis-server /usr/local/etc/redis/redis.conf
+	docker run -it -u 0 -d \
+		-p ${port}:${port} -p 1${port}:1${port} \
+		--privileged=true \
+		-v /home/redis/node-${port}/conf/redis.conf:/usr/local/etc/redis/redis.conf \
+		-v /home/redis/node-${port}/data:/data \
+		--restart always --name redis-${port} --net redis-net \
+		--sysctl net.core.somaxconn=1024 \
+		redis:5.0 redis-server /usr/local/etc/redis/redis.conf
 done
-
+echo "===== Container Done ====="
 
 #### 集群注册 #####
 
@@ -61,8 +64,6 @@ for port in {6401..6406}; do
 	fi
 	${cluster_server}="${cluster_server}${ip}:${port}"
 done
-docker exec -i redis-6379 /bin/bash -c "redis-cli -a ${pwd} --cluster create ${cluster_server} -a ${pwd} --cluster-replicas 1"
-
-
-
+docker exec -i redis-6401 /bin/bash -c "redis-cli -a ${pwd} --cluster create ${cluster_server} -a ${pwd} --cluster-replicas 1"
+echo "===== Bind Done ====="
 
