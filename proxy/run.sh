943049f37d7ed12fd2d52e90cec77d3d507479de
JiangYangfan <jiangyangfan@ifugle.com>
2023-04-21 16:55:22 +0800

    修改：默认配置修改enable-cross-slot 1

--- proxy/run.sh
+++ proxy/run.sh
@@ -9,5 +9,6 @@ for port in {6401..6406}; do
 	fi
 	cluster_server="${cluster_server}${ip}:${port}"
 done
+pwd="GqdLSFUhnm6MpyeVKIu3"
 # 对外6379以单节点方式使用
-docker run -it -d -p 6379:7777 --net redis-net --name redis-cluster-proxy redis-cluster-proxy:v1 redis-cluster-proxy -a GqdLSFUhnm6MpyeVKIu3 ${cluster_server}
+docker run -it -d -p 6379:7777 --net redis-net --name redis-cluster-proxy redis-cluster-proxy:v1 redis-cluster-proxy --enable-cross-slot -a ${pwd} ${cluster_server}
