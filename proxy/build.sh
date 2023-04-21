c48d6a87e0dc1c975f057a6d87c1ca9601517010
JiangYangfan <jiangyangfan@ifugle.com>
2023-04-21 16:13:07 +0800

    修改：遇到Value为空字串时不报错

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,3 +1,4 @@
 # 二次开发过，修复了 Protocol error: invalid bulk length 报错问题
+rm -rf ./redis-cluster-proxy
 git clone https://codeup.aliyun.com/5f707fab3035265285847763/rewrite/redis-cluster-proxy.git
 docker build -t redis-cluster-proxy:v1 . 
