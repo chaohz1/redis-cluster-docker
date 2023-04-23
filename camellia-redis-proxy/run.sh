63f8de6c3c4f41eccf865fc57b1c9a4d625fee2a
JiangYangfan <jiangyangfan@ifugle.com>
2023-04-23 15:49:47 +0800

    新增：camellia-redis-proxy

--- camellia-redis-proxy/run.sh
+++ camellia-redis-proxy/run.sh
@@ -1,4 +1,4 @@
 container_name=camellia-redis-proxy
 docker stop ${container_name}
 docker rm ${container_name}
-docker run -it -d -p 6379:6380 -p 16379:16379 --net redis-net -v ${PWD}/application.yml:/apps/camellia-redis-proxy/BOOT-INF/classes/application.yml --name ${container_name} camellia-redis-proxy:1.2.5 java -XX:+UseG1GC -Xms1024m -Xmx2048m -server org.springframework.boot.loader.JarLauncher
+docker run -it -d -p 6379:6380 -p 16379:16379 --net redis-net -v ${PWD}/application.yml:/apps/camellia-redis-proxy/BOOT-INF/classes/application.yml --name ${container_name} camellia-redis-proxy:1.2.5 java -XX:+UseG1GC -Xms1024m -Xmx2048m -X -XX:+UseContainerSupport -server org.springframework.boot.loader.JarLauncher
